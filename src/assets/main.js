2a25d8e3d6a1350e3117fa56f1e68abc15691dad
paome <poamme@gmail.com>
2018-04-10 11:43:04 +0200

    step5

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,11 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+  function setHiddenFields(){
+    var temp = Math.random()*10000;
+    var answer = Math.floor(temp);
+    return answer;
+  }
 }
 
 
