df8a683dbaa8e32219af5272f8577a33110b7dff
paome <poamme@gmail.com>
2018-04-10 11:45:05 +0200

    tet5

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,11 +4,13 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-  function setHiddenFields(){
-    var temp = Math.random()*10000;
+
+  }
+
+  function setHiddenFields() {
+    var temp = Math.random() * 10000;
     var answer = Math.floor(temp);
     return answer;
-  }
 }
 
 
